60853d41e4ab514ff053c884748e84b676835373
PabloPC <pimubiweb@gmail.com>
2022-01-25 22:03:18 +0100

    initial version structure

---
+++ src/components/todolist.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+      todolist goes here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todolist',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
